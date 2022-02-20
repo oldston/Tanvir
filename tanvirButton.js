a7480777179eb77973d0a2bf2024504f7c644186
oldston <98948084+oldston@users.noreply.github.com>
2022-02-21 04:00:48 +0600

    Add files via upload

--- tanvirButton.js
+++ tanvirButton.js
@@ -1,5 +1,5 @@
-let button = document.getElementsByClassName('logo')[0];
+let button1 = document.getElementsByClassName('logo')[0];
 let loginForm = document.getElementsByClassName('loginSection')[0];
-button.addEventListener('click',()=>{
+button1.addEventListener('click',()=>{
     loginForm.classList.toggle('Enable');
 });
